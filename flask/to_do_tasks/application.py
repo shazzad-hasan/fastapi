cc696e36599f38f8b9896de94b9c16c934281d13
Shazzad Hasan <shazzadraihan@gmail.com>
2022-12-26 23:34:55 +0600

    to do list flask

--- flask/to_do_tasks/application.py
+++ flask/to_do_tasks/application.py
@@ -1,7 +1,21 @@
-from flask import Flask 
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+todos = []
+
 @app.route("/")
-def index():
-    return "Hello, world!"
+def tasks():
+    return render_template("tasks.html", todos=todos)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        todos.append(todo)
+        return redirect("/")
+
+if __name__ == "__main__":
+    app.run()
