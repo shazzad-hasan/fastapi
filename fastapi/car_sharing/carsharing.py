7ad57df9546ddd78843638ac5660d2eb59b37af5
Shazzad Hasan <shazzadraihan@gmail.com>
2023-01-01 22:59:44 +0600

    changed dir name

--- fastapi/car_sharing/carsharing.py
+++ fastapi/car_sharing/carsharing.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import uvicorn
+
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/date")
+def date():
+    return {'date': datetime.now()}
+
+if __name__ == "__main__":
+    uvicorn.run("carsharing:app", reload=True)
