25c0714d142d6c19bbe17cb1184896fcffb3d654
Shazzad Hasan <shazzadraihan@gmail.com>
2023-01-20 04:03:09 +0600

    updated

--- fastapi/car_sharing/carsharing.py
+++ fastapi/car_sharing/carsharing.py
@@ -5,13 +5,26 @@ from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 3, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 3, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 8, "size": "l", "fuel": "diesel", "doors": 3, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "manual"},
+]
 
-@app.get("/date")
-def date():
-    return {'date': datetime.now()}
+@app.get("/")
+def welcome(name):
+    return {"message": f"Hi {name}, welcome to the car sharing service!"}
+
+@app.get("/api/cars")
+def get_cars():
+    return db
+
 
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
