2ca2622e6b27eeed459cad7bd88588926c6d908b
Shazzad Hasan <shazzadraihan@gmail.com>
2023-01-23 20:25:12 +0600

    added cars by id

--- fastapi/car_sharing/carsharing.py
+++ fastapi/car_sharing/carsharing.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 from datetime import datetime
@@ -22,7 +22,7 @@ def welcome(name):
     return {"message": f"Hi {name}, welcome to the car sharing service!"}
 
 @app.get("/api/cars")
-def get_cars(size: str|None = None, doors: int|None = None) -> list:
+def get_cars(size: str|None=None, doors: int|None=None) -> list:
     
     if size:
         return [car for car in db if car['size'] == size]
@@ -31,6 +31,14 @@ def get_cars(size: str|None = None, doors: int|None = None) -> list:
 
     return db
 
+@app.get("/api/cars/{id}")
+def car_by_id(id: int) -> dict:
+    result = [car for car in db if car['id'] == id]
+
+    if result: 
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id = {id}.")
 
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
