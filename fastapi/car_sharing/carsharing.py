78b32e4ca3bc8e13ac98b6c7890c85aff2c6c8b0
Shazzad Hasan <shazzadraihan@gmail.com>
2023-01-26 16:16:53 +0600

    food order app

--- fastapi/car_sharing/carsharing.py
+++ fastapi/car_sharing/carsharing.py
@@ -16,13 +16,11 @@ def get_cars(size: str|None=None, doors: int|None=None) -> list:
         return [car for car in db if car.size == size]
     if doors:
         return [car for car in db if car.doors >= doors]
-
     return db
 
 @app.get("/api/cars/{id}")
 def car_by_id(id: int) -> dict:
     result = [car for car in db if car.id == id]
-
     if result: 
         return result[0]
     else:
@@ -30,36 +28,22 @@ def car_by_id(id: int) -> dict:
 
 @app.post("/api/cars/", response_model=CarOutput)
 def add_car(car: CarInput) -> CarOutput:
-    new_car = CarOutput(size=car.size, 
-                        doors=car.doors, 
-                        fuel=car.fuel, 
-                        transmission=car.transmission, 
-                        id=len(db)+1)
+    new_car = CarOutput(size=car.size,
+                        doors = car.doors,
+                        fuel = car.fuel,
+                        transmission=car.transmission,
+                        id = len(db)+1)
     db.append(new_car)
     save_db(db)
     return new_car 
 
 @app.delete("/api/cars/{id}", status_code=204)
 def remove_car(id: int) -> None:
-    matches = [car for car in db if car.id == id]
+    matches = [car for car in db if car.id==id]
     if matches:
         car = matches[0]
         db.remove(car)
         save_db(db)
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
-
-@app.put("/api/cars/{id}", response_model=CarOutput)
-def change_car(id: int, new_data: CarInput) -> CarOutput:
-    matches = [car for car in db if car.id == id]
-    if matches:
-        car = matches[0]
-        car.fuel = new_data.fuel 
-        car.transmission = new_data.transmission 
-        car.size = new_data.size 
-        car.doors = new_data.doors 
-        save_db(db)
-        return car 
     else:
         raise HTTPException(status_code=404, detail=f"No car with id={id}.")
 
--- fastapi/car_sharing/schemas.py
+++ fastapi/car_sharing/schemas.py
@@ -8,13 +8,14 @@ class CarInput(BaseModel):
     transmission: str | None = "auto"
 
 class CarOutput(CarInput):
-    id: int
+    id: int 
 
 def load_db() -> list[CarOutput]:
-    """ Load a list of car objects from a JSON file """
     with open("cars.json") as f:
-        return [CarInput.parse_obj(obj) for obj in json.load(f)]
+        return [CarOutput.parse_obj(obj) for obj in json.load(f)]
 
 def save_db(cars: list[CarOutput]):
-    with open("cars.json", "w") as f:
+    with open("cars.json", 'w') as f:
         json.dump([car.dict() for car in cars], f, indent=4)
+
+
